a53b777bad40fc9e69882c90fd256d2290c3f1a2
“fridalebo” <aleija@centro.edu.mxgit config --global user.name fridalebogit config --global user.email aleija@centro.edu.mx>
2025-10-08 10:57:29 -0600

    definición de variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nombre = "frida";
+let edad = 23;
+const PI = math.PI;
+const TAU = math.PI * 2;
+
+let esMAyorDeEdad = edad > edad >= 18;
+
+nombre = "frida";
+edad ="23";
+console.log("hola" + nombre);
