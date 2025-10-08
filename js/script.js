2a7076552f33e45f3cbb36bc6d462ae7476bf070
“fridalebo” <aleija@centro.edu.mxgit config --global user.name fridalebogit config --global user.email aleija@centro.edu.mx>
2025-10-08 11:35:01 -0600

    definición de función

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var nombre = "frida";
 let edad = 23;
-const PI = math.PI;
-const TAU = math.PI * 2;
+const PI = Math.PI
+const TAU = Math.PI * 2;
 
 let esMAyorDeEdad = edad > edad >= 18;
 
 nombre = "frida";
 edad ="23";
-console.log("hola" + nombre);
+console.log("hola " + nombre);
+
+function saludar(_nombre="Mundo") {
+    alert("hola " + _nombre);
+}
+
+saludar();
+saludar(nombre);
+saludar("Ana");
