3cadf4a575b5c9807e10aca8bea791ce2780e57e
“fridalebo” <aleija@centro.edu.mxgit config --global user.name fridalebogit config --global user.email aleija@centro.edu.mx>
2025-10-13 20:38:13 -0600

    If, else activar

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,10 +1,28 @@
 const toggleThemeBtn = document.getElementById("toggle-theme-btn");
+const colorPicker = document.getElementById("color-picker");
+const bgPicker = document.getElementById("bg-color-picker");
 
 document.body.classList.add("clase-ejemplo");
 
 toggleThemeBtn.addEventListener("click", function() {
 
     document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+        toggleThemeBtn.textContent = "Activar Light Mode"; 
+
+    } else {
+        toggleThemeBtn.textContent = "Activar Dark Mode";
+
+    }
    // alert("me diste click");
 }
 );
+
+colorPicker.addEventListener("change", function() {
+    document.body.style.color = colorPicker.value;
+});
+
+bgPicker.addEventListener("change", function() {
+    document.body.style.background = bgPicker.value;
+    });
+    
