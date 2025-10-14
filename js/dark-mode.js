0a1cee74222fd9cc17679f5cc26296ee71945316
“fridalebo” <aleija@centro.edu.mxgit config --global user.name fridalebogit config --global user.email aleija@centro.edu.mx>
2025-10-13 19:24:56 -0600

    yes

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,6 @@
 const toggleThemeBtn = document.getElementById("toggle-theme-btn");
 
 toggleThemeBtn.addEventListener("click", function() {
-    alert("me diste click");
+   // alert("me diste click");
 }
 )
