de2c78fa53cedb0f64252281867e18dd3e68e605
“fridalebo” <aleija@centro.edu.mxgit config --global user.name fridalebogit config --global user.email aleija@centro.edu.mx>
2025-10-15 10:55:57 -0600

    Iconos

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -7,10 +7,13 @@ document.body.classList.add("clase-ejemplo");
 function toggleTheme(){
     document.body.classList.toggle("dark");
     if(document.body.classList.contains("dark")){
-        toggleThemeBtn.textContent = "Activar Light Mode"; 
+        toggleThemeBtn.innerHTML =" <i class='fa-solid fa-sun'></i>";
+
+     //toggleThemeBtn.textContent = "Activar Light Mode"; 
 
     } else {
-        toggleThemeBtn.textContent = "Activar Dark Mode";
+        toggleThemeBtn.innerHTML = "<i class='fa-solid fa-moon'></i>";
+        // toggleThemeBtn.textContent = "Activar Dark Mode";
 
     }
    // alert("me diste click");
